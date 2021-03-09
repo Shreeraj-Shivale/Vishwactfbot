29bea17e6bd138ab909d944155dc8d906e6b6a99
Aditya Bakare <adityab7@protonmail.com>
2021-03-09 19:01:35 +0530

    Move Bot Token to main.py, add requirements.txt
    Signed-off-by: Aditya Bakare <adityab7@protonmail.com>

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import discord
-import os
 
 client = discord.Client()
 
+TOKEN = "ODE4NDc4MDM0MzczNzA1NzUy.YEYpEQ.I8x7hntywY3f4yJ5vU3S3yqPKJw"
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$flag'):
-    await message.channel.send('vishwactf{discord_spilled_the_beans}')
+    if message.content.startswith('$flag'):
+        await message.channel.send('vishwactf{discord_spilled_the_beans}')
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('vishwactf{hello}')
+    if message.content.startswith('$hello'):
+        await message.channel.send('vishwactf{hello}')
 
-client.run(os.getenv('TOKEN'))
 
+client.run(TOKEN)
