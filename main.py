bd6c57bbc8df5b388804b30d262ba56c949c5d8b
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-03-09 17:55:51 +0530

    basic bot

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$flag'):
+    await message.channel.send('vishwactf{discord_spilled_the_beans}')
+
+client.run(os.getenv('TOKEN'))
+
